c7e9beec97f5d7fb5647c683a815de342c42483b
Ashutosh Pradhan <119951421+ashutoshpradhan3@users.noreply.github.com>
2025-07-03 19:39:04 +0530

    database integration with MongoDb

--- models/home.js
+++ models/home.js
@@ -1,35 +1,52 @@
-// Core Modules
-const db = require("../utils/databaseUtil");
+const { ObjectId } = require('mongodb');
+const { getDB } = require('../utils/databaseUtil');
 
 module.exports = class Home {
-  constructor(houseName, price, location, rating, photoUrl, description, id) {
+  constructor(houseName, price, location, rating, photoUrl, description, _id) {
     this.houseName = houseName;
     this.price = price;
     this.location = location;
     this.rating = rating;
     this.photoUrl = photoUrl;
     this.description = description;
-    this.id = id;
+    if (_id) {
+      this._id = _id;
+    }
   }
 
   save() {
-    if (this.id) { // update
-      return db.execute('UPDATE homes SET houseName=?, price=?, location=?, rating=?, photoUrl=?, description=? WHERE id=?', [this.houseName, this.price, this.location, this.rating, this.photoUrl, this.description, this.id]);
+    const db = getDB();
+    if (this._id) { // update
+      const updateFields = {
+        houseName: this.houseName,
+        price: this.price,
+        location: this.location,
+        rating: this.rating,
+        photoUrl: this.photoUrl,
+        description: this.description
+      };
 
+      return db.collection('homes').updateOne({_id: new ObjectId(String(this._id))}, {$set: updateFields});
     } else { // insert
-      return db.execute('INSERT INTO homes (houseName, price, location, rating, photoUrl, description) VALUES (?, ?, ?, ?, ?, ?)', [this.houseName, this.price, this.location, this.rating, this.photoUrl, this.description]);
+      return db.collection('homes').insertOne(this);
     }
   }
 
   static fetchAll() {
-    return db.execute('SELECT * FROM homes');
+    const db = getDB();
+    return db.collection('homes').find().toArray();
   }
 
   static findById(homeId) {
-    return db.execute('SELECT * FROM homes WHERE id=?', [homeId]);
+    const db = getDB();
+    return db.collection('homes')
+    .find({_id: new ObjectId(String(homeId))})
+    .next();
   }
 
   static deleteById(homeId) {
-    return db.execute('DELETE FROM homes WHERE id=?', [homeId]);
+    const db = getDB();
+    return db.collection('homes')
+    .deleteOne({_id: new ObjectId(String(homeId))});
   }
 };
--- utils/databaseUtil.js
+++ utils/databaseUtil.js
@@ -1,10 +1,27 @@
-const mysql = require('mysql2');
+const mongo = require('mongodb');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Ashu1234@@',
-  database: 'airbnb'
-});
+const MongoClient = mongo.MongoClient;
 
-module.exports = pool.promise();
+const MONGO_URL = "mongodb+srv://root:Ashu1234@stayhub.zajasqe.mongodb.net/?retryWrites=true&w=majority&appName=StayHub";
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(MONGO_URL)
+  .then(client => {
+    callback();
+    _db = client.db('airbnb');
+  }).catch(err => {
+    console.log('Error while connecting to Mongo: ', err);
+  });
+}
+
+const getDB = () => {
+  if (!_db) {
+    throw new Error('Mongo not connected');
+  }
+  return _db;
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
